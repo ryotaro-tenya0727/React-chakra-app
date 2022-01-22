adfa5af771c2b3a4137e0f0031b8f6edfd6f5a4d
中山遼太郎 <71915489+ryotaro-tenya0727@users.noreply.github.com>
2022-01-22 14:56:12 +0900

    【追加】コンポーネントを分割

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,20 @@
+import { Flex, Box, Divider, Input, Heading, Stack } from "@chakra-ui/react";
 import { memo, VFC } from "react";
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
 
 export const Login: VFC = memo(() => {
-  return <p>ログイン</p>;
+  return (
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">
+          ユーザー管理アプリ
+        </Heading>
+        <Divider my={4} />
+        <Stack spacing={8} py={4} px={10}>
+          <Input placeholder="ユーザーID" />
+          <PrimaryButton>ログイン</PrimaryButton>
+        </Stack>
+      </Box>
+    </Flex>
+  );
 });
