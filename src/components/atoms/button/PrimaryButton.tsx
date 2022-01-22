e55efd5dbdcd0b6ea05222ff8288bd765bd30711
中山遼太郎 <71915489+ryotaro-tenya0727@users.noreply.github.com>
2022-01-22 15:38:49 +0900

    【追加】ログイン機能追加

--- src/components/atoms/button/PrimaryButton.tsx
+++ src/components/atoms/button/PrimaryButton.tsx
@@ -4,12 +4,24 @@ import { Button } from "@chakra-ui/react";
 
 type Props = {
   children: ReactNode;
+  disabled?: boolean;
+  loading?: boolean;
+  onClick: () => void;
 };
 
-export const PrimaryButton: VFC<Props> = memo(({ children }) => {
-  return (
-    <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
-      {children}
-    </Button>
-  );
-});
+export const PrimaryButton: VFC<Props> = memo(
+  ({ children, onClick, disabled = false, loading = false }) => {
+    return (
+      <Button
+        bg="teal.400"
+        color="white"
+        _hover={{ opacity: 0.8 }}
+        disabled={disabled || loading}
+        isLoading={loading}
+        onClick={onClick}
+      >
+        {children}
+      </Button>
+    );
+  }
+);
