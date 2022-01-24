453fc1f4f21bb9454042f73e5d46b9826f8a9e97
中山遼太郎 <71915489+ryotaro-tenya0727@users.noreply.github.com>
2022-01-24 09:06:26 +0900

    【追加】メッセージ表示機能追加

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,8 +3,11 @@ import { useCallback, useState } from "react";
 import { User } from "./../types/api/user";
 import { useNavigate } from "react-router-dom";
 
+import { useMessage } from "./useMessage";
+
 export const useAuth = () => {
   const navigate = useNavigate();
+  const { showMessage } = useMessage();
 
   const [loading, setLoading] = useState(false);
 
@@ -14,12 +17,15 @@ export const useAuth = () => {
       .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
       .then((res) => {
         if (res.data) {
+          showMessage({ title: "ログインしました", status: "success" });
           navigate("/home");
         } else {
-          alert("ユーザーが見つかりません");
+          showMessage({ title: "ユーザーが見つかりません", status: "error" });
         }
       })
-      .catch(() => alert("ログインできません"))
+      .catch(() => {
+        showMessage({ title: "ログインできません", status: "error" });
+      })
       .finally(() => {
         setLoading(false);
       });
