f452d0e6f49f1ce63ad2db73001a43a6f7b7c1aa
中山遼太郎 <71915489+ryotaro-tenya0727@users.noreply.github.com>
2022-01-24 11:26:42 +0900

    【追加】ユーザー詳細モーダル画面の作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,15 +1,25 @@
-import { memo, VFC, useEffect } from "react";
-import { Wrap, WrapItem, Spinner, Center } from "@chakra-ui/react";
+import { memo, VFC, useEffect, useCallback } from "react";
+import {
+  Wrap,
+  WrapItem,
+  Spinner,
+  Center,
+  useDisclosure
+} from "@chakra-ui/react";
 
 import { UserCard } from "./../organisms/user/UserCard";
 import { useAllUsers } from "../../hooks/useAllUsers";
+import { UserDetailModal } from "../organisms/user/UserDetailModal";
 
 export const UserManagement: VFC = memo(() => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { getUsers, users, loading } = useAllUsers();
 
   useEffect(() => {
     getUsers();
   }, []);
+
+  const onClickUser = useCallback(() => onOpen(), []);
 
   return (
     <>
@@ -18,18 +28,20 @@ export const UserManagement: VFC = memo(() => {
           <Spinner />
         </Center>
       ) : (
-        <Wrap p={{ base: 4, md: 10 }}>
+        <Wrap p={{ base: 4, md: 10 }} justify="center">
           {users.map((user) => (
             <WrapItem key={user.id}>
               <UserCard
                 imageUrl="https://source.unsplash.com/random"
                 userName={user.username}
                 fullName={user.name}
+                onClick={onClickUser}
               />
             </WrapItem>
           ))}
         </Wrap>
       )}
+      <UserDetailModal isOpen={isOpen} onClose={onClose} />
     </>
   );
 });
